33f35e9430d3c925f5faa4ac6a20361dc00e48da
admin <admin@admin-PC>
2017-11-22 20:12:27 +0530

    Added -and update lines

--- TestForGit/src/sushPackage/MyClassOne.java
+++ TestForGit/src/sushPackage/MyClassOne.java
@@ -2,13 +2,14 @@ package sushPackage;
 
 public class MyClassOne {
 	
-	String su="Patil11";
+	String su="Sushant Patil11";
 
 	public static void main(String[] args) {
 		System.out.println("New One");
 		System.out.println("New Two");
 		System.out.println("New Three");
-
+	
+		String su="Sushant Patil11---2 Line ";
 	}
 
 }
