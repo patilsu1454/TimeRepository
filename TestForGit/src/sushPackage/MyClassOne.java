4c515f403c63b25f8bc324e56c1d8bb7c59ea394
admin <admin@admin-PC>
2017-11-22 19:47:45 +0530

    Committing and push TestForGit Project

---
+++ TestForGit/src/sushPackage/MyClassOne.java
@@ -0,0 +1,14 @@
+package sushPackage;
+
+public class MyClassOne {
+	
+	String su="Patil";
+
+	public static void main(String[] args) {
+		System.out.println("New One");
+		System.out.println("New Two");
+		System.out.println("New Three");
+
+	}
+
+}
---
+++ TestForGit/src/sushPackageTwo/MyClassTwo.java
@@ -0,0 +1,13 @@
+package sushPackageTwo;
+
+public class MyClassTwo {
+
+	
+	public static void main(String[] args) {
+		System.out.println("New One1");
+		System.out.println("New Two2");
+		System.out.println("New Three3");
+
+	}
+
+}
